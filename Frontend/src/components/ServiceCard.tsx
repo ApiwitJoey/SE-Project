8d9a936fcd504471114e9da7bdf96b7ab1178539
Pannawich Thamart <MadManJJ4949@gmail.com>
2025-04-21 10:47:42 +0700

    Add ConfirmationPopup to ServiceCard on delete service

--- Frontend/src/components/ServiceCard.tsx
+++ Frontend/src/components/ServiceCard.tsx
@@ -1,7 +1,11 @@
 import { Service } from "../../interfaces"
+import ConfirmationPopup from "./ConfirmPopup"
+import { useState } from "react";
 
 const ServiceCard = ({service, isEditable, deleteOnclick, editOnclick} : {service: Service, isEditable:boolean, deleteOnclick?:Function, editOnclick?:Function}) => {
-  return (
+    const [showPopup, setShowPopup] = useState(false);
+
+    return (
     <div
         key={service._id}
         className="bg-emerald-50 rounded-lg p-5 border border-emerald-100 hover:shadow-md transition-shadow"
@@ -29,7 +33,7 @@ const ServiceCard = ({service, isEditable, deleteOnclick, editOnclick} : {servic
                         Edit
                     </button>
                     
-                    <button onClick={(e) => {deleteOnclick(service._id)}} className="flex w-fit px-4 py-2 bg-red-100 text-red-700 rounded-md hover:bg-red-200 transition-colors">
+                    <button onClick={(e) => {setShowPopup(true)}} className="flex w-fit px-4 py-2 bg-red-100 text-red-700 rounded-md hover:bg-red-200 transition-colors">
                         <svg className="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6M9 7h6m-7 0h8a1 1 0 001-1V5a1 1 0 00-1-1H8a1 1 0 00-1 1v1z" />
                         </svg>
@@ -38,6 +42,15 @@ const ServiceCard = ({service, isEditable, deleteOnclick, editOnclick} : {servic
                 </div>
             )
         }
+
+        {
+            showPopup && isEditable && deleteOnclick && editOnclick && (
+                <ConfirmationPopup
+                    title={`Are you sure you want to delete ${service.name} service?`}
+                    message="this service will be gone forever, there is no redo."
+                    onConfirm={() => {deleteOnclick(service._id); setShowPopup(false)}} 
+                    onCancel={()=>setShowPopup(false)}/>
+        )}
     </div>
   )
 }
