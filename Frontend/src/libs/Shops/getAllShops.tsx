12fabd10d292e522a3f68c606cd97fbe4814091f
Weerawit Pungpiboon <weerawitweem@gmail.com>
2025-04-22 22:08:31 +0700

    fix get shops api url

--- Frontend/src/libs/Shops/getAllShops.tsx
+++ Frontend/src/libs/Shops/getAllShops.tsx
@@ -2,7 +2,7 @@ import { ShopJson } from "../../../interfaces";
 
 export default async function getAllShops(queryString: string = ""): Promise<ShopJson> {
   try {
-    const response = await fetch(`${process.env.BACKEND_URL}/shops${queryString ? `?${queryString}` : ""}`);
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/shops${queryString ? `?${queryString}` : ""}`);
     
     if (!response.ok) {
       throw new Error(`Error: ${response.status}`);
