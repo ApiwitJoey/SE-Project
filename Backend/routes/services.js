cc6c9a2c4a8e55ff73479f5c9741dcd549a6f86e
ApwitJoey <joeyapiwtch@gmail.com>
2025-04-23 23:56:33 +0700

    Change getservices & getservice to public

--- Backend/routes/services.js
+++ Backend/routes/services.js
@@ -13,8 +13,8 @@ const {
 
 // Protect is used to check if the user is logged in
 // authorize checks if the user has the required role
-router.get("/:id", protect, getService);
-router.get("/", protect, getServices);
+router.get("/:id", getService);
+router.get("/", getServices);
 router.post("/", protect, authorize("admin", "user"), createService); // from shops first
 router.put("/:id", protect, authorize("admin", "user"), updateService);
 router.delete("/:id", protect, authorize("admin", "user"), deleteService);
