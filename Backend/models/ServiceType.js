c797fc140b28233fab9bcde90935e5c05506a38d
ApwitJoey <joeyapiwtch@gmail.com>
2025-04-25 00:03:27 +0700

    Fix ServiceType

--- Backend/models/ServiceType.js
+++ Backend/models/ServiceType.js
@@ -1,13 +1,27 @@
 // utils/enums.js
 const targetAreas = [
-    'Head & Shoulder', 'Foot', 'Neck-Shoulder-Back', 
-    'Chair', 'Abdominal', 'Hand & Arm', 'Leg', 'Full Body'
+    'Head & Shoulder', 
+    'Foot', 
+    'Neck-Shoulder-Back', 
+    'Chair', 
+    'Abdominal', 
+    'Hand & Arm', 
+    'Leg', 
+    'Full Body'
   ];
   
   const massageTypes = [
-    'Thai', 'Swedish', 'Oil/Aromatherapy', 'Herbal Compress',
-    'Deep Tissue', 'Sports', 'Office Syndrome',
-    'Shiatsu', 'Lomi-Lomi', 'Trigger Point', 'Others'
+    'Thai', 
+    'Swedish', 
+    'Oil/Aromatherapy', 
+    'Herbal Compress',
+    'Deep Tissue', 
+    'Sports', 
+    'Office Syndrome',
+    'Shiatsu', 
+    'Lomi-Lomi', 
+    'Trigger Point',
+    'Others'
   ];
   
   module.exports = { targetAreas, massageTypes };  
