c38849880c21750ff632ece9a0ba8780c733d0b2
watthanakorn Rakthong <watthanakorn.r@gmail.com>
2025-04-24 14:15:25 +0700

    fix confirm color

--- Frontend/src/components/ConfirmPopup.tsx
+++ Frontend/src/components/ConfirmPopup.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 interface ConfirmationPopupProps {
     title: string;
     message: string;
@@ -7,6 +9,10 @@ interface ConfirmationPopupProps {
   }
   
 const ConfirmationPopup: React.FC<ConfirmationPopupProps> = ({ title, message, onConfirm, onCancel, confirmColor}) => (
+
+
+
+
 <div className="fixed inset-0 flex items-center justify-center bg-black/50 backdrop-blur-sm z-50">
     <div className="bg-white p-6 rounded-lg shadow-lg">
     <p className="text-green-900 text-4xl font-bold">{title}</p>
@@ -14,7 +20,7 @@ const ConfirmationPopup: React.FC<ConfirmationPopupProps> = ({ title, message, o
     <div className="mt-4">
         <button
         onClick={onConfirm}
-        className={`mr-2 px-4 py-2 text-white bg-red-600 bg-${confirmColor}-600 hover:bg-red-700 hover:bg-${confirmColor}-700 rounded`}
+        className={`mr-2 px-4 py-2 text-white bg-${confirmColor??'red'}-600 hover:bg-${confirmColor??'red'}-700 rounded`}
         >
         Confirm
         </button>
