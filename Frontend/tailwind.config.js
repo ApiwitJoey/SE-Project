d88b58141567b1dede929b193f3b9a163d5d303d
Pannawich Thamart <MadManJJ4949@gmail.com>
2025-04-26 23:21:39 +0700

    Fix tailwind color bug

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}'
   ],
+  safelist: [
+    "bg-green-600",
+    "bg-red-600",
+    "hover:bg-green-700", 
+    "hover:bg-red-700",
+    "text-green-600", 
+    "text-red-600",
+    "border-green-600", 
+    "border-red-600",
+  ],
   theme: {
     extend: {
       fontFamily: {
